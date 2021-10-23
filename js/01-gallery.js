5d92bc43d7aaa690d719ef26cb8503f9440fc00b
MykolaKurin <nvk1967@gmail.com>
2021-10-23 18:35:14 +0300

    1_task_update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+const createImg = galleryItems.map(image =>
+  `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`
+).join("");
+
+list.insertAdjacentHTML('afterbegin', createImg);
 console.log(galleryItems);
