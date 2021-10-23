2d1d8a5e5d7b6a98aebcd02532de74f65f9df5f9
MykolaKurin <nvk1967@gmail.com>
2021-10-23 23:27:18 +0300

    Task2_update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,5 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 var lightbox = new SimpleLightbox('.gallery');
+    lightbox.on('show.simplelightbox', function () {
+	// do something…
+});
+
+const openModal = (event) => {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img class="gallery__image" src="${event.target.dataset.source}" alt="Some text"/>
+    </div>
+  `);
+
+  instance.show();
+}
+
+const createImg = galleryItems.map(image =>
+  `<div class="gallery__item">
+    <a class="gallery__item" href="${image.original}">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+    </a>
+  </div>`
+).join("");
+
+
+//lightbox.insertAdjacentHTML('afterbegin', createImg);
+//lightbox.addEventListener('click', openModal);
 
 console.log(galleryItems);
