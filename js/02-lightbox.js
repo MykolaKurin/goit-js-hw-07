716f95b14c3b66852712b89ea1012f1a1bc87fd8
MykolaKurin <nvk1967@gmail.com>
2021-10-24 21:53:01 +0300

    Update library connections

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-//var lightbox = new SimpleLightbox(".gallery");
 
 const galeryRef = document.querySelector(".gallery");
 
