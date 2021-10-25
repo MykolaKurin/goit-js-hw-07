940d410a56314b66d346452c9097b25501e2eefb
MykolaKurin <nvk1967@gmail.com>
2021-10-25 22:33:53 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galeryRef = document.querySelector(".gallery");
-const openModal = (event) => {
-  event.preventDefault();
 
-  const instance = basicLightbox.create(`
-   
-    <div class="gallery">
-        <img class="gallery__image" src="${event.target.dataset.source}" alt="Some text"
-        />
-        </a>
-    </div>
-  `);
-
-  instance.show();
-}
 
 const createImg = galleryItems.map(image =>
   `<div class="gallery__item">
@@ -28,9 +15,8 @@ const createImg = galleryItems.map(image =>
 ).join("");
 
 galeryRef.insertAdjacentHTML('afterbegin', createImg);
-galeryRef.addEventListener('click', openModal);
 
-new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
 console.log(galleryItems);
 
 
